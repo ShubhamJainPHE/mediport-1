4b87e69b88c2e8ab4860c04a9373a8b5b0573d6a
akul <akul.gupta.phe17@itbhu.ac.in>
2020-03-27 01:34:23 +0530

    covid19-6

--- mediport/urls.py
+++ mediport/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('superuser@mediport.info/', admin.site.urls),
     path('', include('medapp.urls')),
 ]
 if settings.DEBUG:
